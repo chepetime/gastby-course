946fbc3c5ef684759324265342aabccfd17e7176
José Lugo <chepe.time@gmail.com>
2020-09-28 21:33:34 -0500

    Added CSS subgrid

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,34 +1,57 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-function SinglePizza({ pizza: { id, name, price, slug, toppings, image } }) {
+const PizzaGridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+  /* Take your row sizing not from the pizzaStyles div, but from the  PizzaGridStyles grid */
+  @supports not (grid-template-rows: subgrid) {
+    --rows: auto auto 1fr;
+  }
+  grid-template-rows: var(--rows, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
+
+function SinglePizza({ pizza: { name, price, slug, toppings, image } }) {
   return (
-    <div>
+    <PizzaStyles>
       <Link to={`/pizzas/${slug.current}`}>
-        <h2 className="mark">{name}</h2>
-
-        <p>{price}</p>
-        <p>{slug.current}</p>
-        <ul>
-          {toppings
-            .map(
-              ({ name: nameTop, vegetarian }) =>
-                `${nameTop} ${vegetarian ? ' 🌱' : ''}`
-            )
-            .join(', ')}
-        </ul>
-        <img src={image.asset.fluid.base64} alt="" />
+        <h2 className="mark">
+          {name} <span>{price}</span>
+        </h2>
       </Link>
-    </div>
+      <p>
+        {toppings
+          .map(
+            ({ name: nameTop, vegetarian }) =>
+              `${nameTop} ${vegetarian ? ' 🌱' : ''}`
+          )
+          .join(', ')}
+      </p>
+      <Img fluid={image.asset.fluid} alt={name} />
+    </PizzaStyles>
   );
 }
 
 export default function PizzaList({ pizzas }) {
   return (
-    <div>
+    <PizzaGridStyles>
       {pizzas.map((pizza) => (
         <SinglePizza key={`single-pizza-${pizza.id}`} pizza={pizza} />
       ))}
-    </div>
+    </PizzaGridStyles>
   );
 }
--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -6,7 +6,7 @@ import PizzaList from '../components/PizzaList';
 export default function PizzasPage({ data }) {
   return (
     <>
-      <PizzaList pizzas={data.pizzas.nodes} />
+      {data && <PizzaList pizzas={data.pizzas.nodes} />}
       <p>Hey, there are {data.pizzas.nodes.length} pizas page</p>
     </>
   );
@@ -29,6 +29,9 @@ export const query = graphql`
         }
         image {
           asset {
+            fixed(width: 200, height: 200) {
+              ...GatsbySanityImageFixed
+            }
             fluid(maxWidth: 400) {
               ...GatsbySanityImageFluid
             }
